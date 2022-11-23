5d983d89f31dc3735d69ff7076d2e4d93180365f
goyois <bau12288@gmail.com>
2022-11-23 22:53:20 +0900

    Test

--- src/main/java/com/sns/member/domain/service/MemberService.java
+++ src/main/java/com/sns/member/domain/service/MemberService.java
@@ -1,4 +1,23 @@
-package com.sns.member.domain.service;
-
-public class MemberService {
-}
+//package com.sns.member.domain.service;
+//
+//import com.sns.member.domain.entity.Member;
+//
+//public class MemberService {
+//
+//    public Member createMembe(Member member) {
+//        Member createdMember = member;
+//        return createdMember;
+//    }
+//
+//    public Member updateMember(Member member) {
+//        Member updateMember = member;
+//        return updateMember;
+//    }
+//
+//    public Member findMember(long memberId;) {
+//        return null;
+//    }
+//    public void deleteMember(long memberId) {
+//
+//    }
+//}

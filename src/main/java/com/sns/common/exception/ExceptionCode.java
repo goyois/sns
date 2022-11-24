dfba1e9273ccc62d0ade8c8d680ecdf9a288ddd2
goyois <bau12288@gmail.com>
2022-11-24 22:29:27 +0900

    [Style] : ExceptionCode enum 추가

--- src/main/java/com/sns/common/exception/ExceptionCode.java
+++ src/main/java/com/sns/common/exception/ExceptionCode.java
@@ -4,7 +4,10 @@ import lombok.Getter;
 
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404,"Member Not Found"),
-    MEMBER_EXISTS(409,"Member Exists");
+    MEMBER_EXISTS(409,"Member Exists"),
+    POST_NOT_FOUND(404,"Post Not Found"),
+    COMMENT_NOT_FOUND(404,"Comment Not Found");
+
 
     @Getter
     private int status;

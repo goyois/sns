a99234c596220a1a850b417ae8a3c25d2c05b75f
goyois <bau12288@gmail.com>
2023-01-27 19:10:28 +0900

    [Chore] SpringbootTest 제거

--- server/src/test/java/com/sns/SnsApplicationTests.java
+++ server/src/test/java/com/sns/SnsApplicationTests.java
@@ -3,7 +3,7 @@ package com.sns;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SnsApplicationTests {
 
 	@Test
